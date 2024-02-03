62a2a97aef784e278dc4c5ef6e69606ac14975bb
KindLab <qywxb@qq.com>
2024-02-03 20:12:57 +0800

    Delete baselines directory

--- baselines/neural_networks/metric_gmean.m
+++
@@ -1,7 +0,0 @@
-function [ gmean ] = metric_gmean(y, yscore)
-    C = confusionmat(y,yscore); % generate confusion matrix
-    true_positives = diag(C); % get the number of true positives
-    actual_positives = sum(C, 2); % get the number of actual positives
-    recall = true_positives ./ actual_positives; % calculate recall
-    gmean = geomean(recall);
-end
